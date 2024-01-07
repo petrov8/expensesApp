3dbacd6324b75717488f2f265005e32c19035f3f
petrov8 <a.petrov08@outlook.com>
2024-01-07 13:31:38 +0200

    final version

---
+++ src/components/Concepts/ConceptBox.jsx
@@ -0,0 +1,14 @@
+
+function ConceptBox({title, image, description}) {
+
+    return (
+        <li className="concept">
+            <img src={image} alt={title} />
+            <h2>TODO: {title}</h2>
+            <p>TODO: {description}</p>
+        </li>
+    )
+}
+
+
+export default ConceptBox
