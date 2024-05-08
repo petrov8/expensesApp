aa2d82b9efcbdccd4f6244e2b9b75fd6ba591e5f
petrov8 <a.petrov08@outlook.com>
2024-05-08 23:18:50 +0300

    adding test file

---
+++ styled-components/test.py
@@ -0,0 +1,17 @@
+Dear Jayne,
+
+During his tenure with ARRACO (April 1, 2019 to September 30, 2022), William Drysdale held the position of IT Engineer and demonstrated exceptional proficiency in managing Windows machines, servers, networks, and financial application setup and troubleshooting. His responsibilities also included IT budget reporting and conducting digital security training for new and existing employees.
+
+William consistently exhibited a high standard of work and dedication to his role. His technical expertise played a crucial role in maintaining the efficiency and functionality of our IT infrastructure, which was pivotal to our operations at ARRACO Global Markets.
+
+Throughout his employment, William maintained a commendable disciplinary record, demonstrating professionalism and adherence to company policies.
+
+Unfortunately, ARRACO Global Markets faced financial challenges that led to the company filing bankruptcy, which led to his departure. His final duties were the rollback of hardware and software internationally across the offices of Dublin, London, Singapore and Tokyo during this period of time.
+
+If given the opportunity, I would not hesitate to rehire him. He proved to be a valuable asset to our team, and his contributions were greatly appreciated during his time with us.
+
+Please feel free to contact me if you require any further information or clarification regarding William Drysdale's performance during his employment at ARRACO Global Markets.
+
+Yours sincerely,
+
+Angel Petrov (former CFO of ARRACO Global Markets)
